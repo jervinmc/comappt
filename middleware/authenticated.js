c691d5c2dcf009325993fcb8d136e411488e0682
jmacalawa <jmacalawa@futurex.ph>
2022-03-29 11:59:59 +0800

    done

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,31 +1,31 @@
-export default async function (context) {
-    if (context.route.name != 'index') {
-        var token = localStorage.getItem('token')
-        const response = await context.$axios.get('', {
-            baseURL: `http://54.164.128.119:5000/api/v1/verify/${token}`, 'headers': {
+// export default async function (context) {
+//     if (context.route.name != 'index') {
+//         var token = localStorage.getItem('token')
+//         const response = await context.$axios.get('', {
+//             baseURL: `http://54.164.128.119:5000/api/v1/verify/${token}`, 'headers': {
 
-            }
-        })
-            .then((response) => {
-                if (!response.data) {
-                    context.redirect('/')
-                }
+//             }
+//         })
+//             .then((response) => {
+//                 if (!response.data) {
+//                     context.redirect('/')
+//                 }
 
-            })
-    }
-    else {
-        var token = localStorage.getItem('token')
-        const response = await context.$axios.get('', {
-            baseURL: `http://54.164.128.119:5000/api/v1/verify/${token}`, 'headers': {
+//             })
+//     }
+//     else {
+//         var token = localStorage.getItem('token')
+//         const response = await context.$axios.get('', {
+//             baseURL: `http://54.164.128.119:5000/api/v1/verify/${token}`, 'headers': {
 
-            }
-        })
-            .then((response) => {
-                if (response.data) {
-                    context.redirect('/engine')
-                }
+//             }
+//         })
+//             .then((response) => {
+//                 if (response.data) {
+//                     context.redirect('/engine')
+//                 }
 
-            })
-    }
+//             })
+//     }
 
-}
+// }
